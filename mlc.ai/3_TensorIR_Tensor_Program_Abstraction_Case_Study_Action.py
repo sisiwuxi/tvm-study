d766c02e3a19a3b8d785904844b910909aa7dc6a
sisiwuxi <89402164@qq.com>
2022-07-03 19:36:02 +0800

    [mlc.ai] Add Episode 3

---
+++ mlc.ai/3_TensorIR_Tensor_Program_Abstraction_Case_Study_Action.py
@@ -0,0 +1,240 @@
+import tvm
+from tvm.ir.module import IRModule
+from tvm.script import tir as T
+import numpy as np
+
+# ==================== lower numpy ==================== #
+dtype = "float32"
+a_np = np.random.rand(128, 128).astype(dtype)
+b_np = np.random.rand(128, 128).astype(dtype)
+# a @ b is equivalent to np.matmul(a, b)
+c_mm_relu = np.maximum(a_np @ b_np, 0)
+
+def lnumpy_mm_relu(A: np.ndarray, B: np.ndarray, C: np.ndarray):
+    Y = np.empty((128, 128), dtype="float32")
+    for i in range(128):
+        for j in range(128):
+            for k in range(128):
+                if k == 0:
+                    Y[i, j] = 0
+                Y[i, j] = Y[i, j] + A[i, k] * B[k, j]
+    for i in range(128):
+        for j in range(128):
+            C[i, j] = max(Y[i, j], 0)
+    return
+
+c_np = np.empty((128, 128), dtype=dtype)
+lnumpy_mm_relu(a_np, b_np, c_np)
+np.testing.assert_allclose(c_mm_relu, c_np, rtol=1e-5)
+print("lnumpy_mm_relu success")
+
+def lnumpy_mm_relu_v2(A: np.ndarray, B: np.ndarray, C: np.ndarray):
+    Y = np.empty((128, 128), dtype="float32")
+    for i in range(128):
+        for j0 in range(32):
+            for k in range(128):
+                for j1 in range(4):
+                    j = j0 * 4 + j1 
+                    if k == 0:
+                        Y[i, j] = 0
+                    Y[i, j] = Y[i, j] + A[i, k] * B[k, j]
+    for i in range(128):
+        for j in range(128):
+            C[i, j] = max(Y[i, j], 0)
+    return
+
+c_np = np.empty((128, 128), dtype=dtype)
+lnumpy_mm_relu_v2(a_np, b_np, c_np)
+np.testing.assert_allclose(c_mm_relu, c_np, rtol=1e-5)
+print("lnumpy_mm_relu_v2 success")
+
+
+def lnumpy_mm_relu_v3(A: np.ndarray, B: np.ndarray, C: np.ndarray):
+    Y = np.empty((128, 128), dtype="float32")
+    for i in range(128):
+        for j0 in range(32):
+            # Y_init
+            for j1 in range(4):
+                j = j0 * 4 + j1 
+                Y[i, j] = 0
+            # Y_update
+            for k in range(128):
+                for j1 in range(4):
+                    j = j0 * 4 + j1 
+                    Y[i, j] = Y[i, j] + A[i, k] * B[k, j]
+            # C
+            for j1 in range(4):
+                j = j0 * 4 + j1 
+                C[i, j] = max(Y[i, j], 0)
+    return
+
+c_np = np.empty((128, 128), dtype=dtype)
+lnumpy_mm_relu_v3(a_np, b_np, c_np)
+np.testing.assert_allclose(c_mm_relu, c_np, rtol=1e-5)
+print("lnumpy_mm_relu_v3 success")
+
+# ==================== ir_module ==================== #
+
+@tvm.script.ir_module
+class MyModule:
+    @T.prim_func
+    def mm_relu(A: T.Buffer[(128, 128), "float32"], 
+                B: T.Buffer[(128, 128), "float32"], 
+                C: T.Buffer[(128, 128), "float32"]):
+        T.func_attr({"global_symbol": "mm_relu", "tir.noalias": True})
+        Y = T.alloc_buffer((128, 128), dtype="float32")
+        for i, j, k in T.grid(128, 128, 128):
+            with T.block("Y"):
+                vi = T.axis.spatial(128, i)
+                vj = T.axis.spatial(128, j)
+                vk = T.axis.reduce(128, k)
+                with T.init():
+                    Y[vi, vj] = T.float32(0)
+                Y[vi, vj] = Y[vi, vj] + A[vi, vk] * B[vk, vj]
+        for i, j in T.grid(128, 128):
+            with T.block("C"):
+                vi = T.axis.spatial(128, i)
+                vj = T.axis.spatial(128, j)             
+                C[vi, vj] = T.max(Y[vi, vj], T.float32(0))
+print(type(MyModule))
+print(type(MyModule["mm_relu"]))
+
+@tvm.script.ir_module
+class MyModuleWithAxisRemapSugar:
+    @T.prim_func
+    def mm_relu(A: T.Buffer[(128, 128), "float32"], 
+                B: T.Buffer[(128, 128), "float32"], 
+                C: T.Buffer[(128, 128), "float32"]):
+        T.func_attr({"global_symbol": "mm_relu", "tir.noalias": True})
+        Y = T.alloc_buffer((128, 128), dtype="float32")
+        for i, j, k in T.grid(128, 128, 128):
+            with T.block("Y"):
+                vi, vj, vk = T.axis.remap("SSR", [i, j, k])
+                with T.init():
+                    Y[vi, vj] = T.float32(0)
+                Y[vi, vj] = Y[vi, vj] + A[vi, vk] * B[vk, vj]
+        for i, j in T.grid(128, 128):
+            with T.block("C"):
+                vi, vj = T.axis.remap("SS", [i, j])
+                C[vi, vj] = T.max(Y[vi, vj], T.float32(0))
+
+print(type(MyModuleWithAxisRemapSugar))
+print(type(MyModuleWithAxisRemapSugar["mm_relu"]))
+
+@tvm.script.ir_module
+class MyModuleWithTwoFunctions:
+    @T.prim_func
+    def mm(A: T.Buffer[(128, 128), "float32"], 
+           B: T.Buffer[(128, 128), "float32"], 
+           Y: T.Buffer[(128, 128), "float32"]):
+        T.func_attr({"global_symbol": "mm", "tir.noalias": True})
+        for i, j, k in T.grid(128, 128, 128):
+            with T.block("Y"):
+                vi, vj, vk = T.axis.remap("SSR", [i, j, k])
+                with T.init():
+                    Y[vi, vj] = T.float32(0)
+                Y[vi, vj] = Y[vi, vj] + A[vi, vk] * B[vk, vj]
+
+    @T.prim_func
+    def relu(A: T.Buffer[(128, 128), "float32"],
+             B: T.Buffer[(128, 128), "float32"]):
+        T.func_attr({"global_symbol": "relu", "tir.noalias": True})
+        for i, j in T.grid(128, 128):
+            with T.block("B"):
+                vi, vj = T.axis.remap("SS", [i, j])
+                B[vi, vj] = T.max(A[vi, vj], T.float32(0))
+print(type(MyModuleWithTwoFunctions))
+print(type(MyModuleWithTwoFunctions["mm"]))
+print(type(MyModuleWithTwoFunctions["relu"]))
+
+
+
+# ==================== sch ==================== #
+import IPython
+def code2html(code):
+    """Helper function to use pygments to turn the code string into highlighted html."""
+    import pygments
+    from pygments.lexers import Python3Lexer
+    from pygments.formatters import HtmlFormatter
+    formatter = HtmlFormatter()
+    html = pygments.highlight(code, Python3Lexer(), formatter)
+    return "<style>%s</style>%s\n" % (formatter.get_style_defs(".highlight"), html)
+
+# IPython.display.HTML(code2html(MyModule.script()))
+print(MyModule.script())
+sch = tvm.tir.Schedule(MyModule)
+block_Y = sch.get_block("Y", func_name="mm_relu")
+i, j, k = sch.get_loops(block_Y)
+j0, j1 = sch.split(j, factors=[None, 4])
+# IPython.display.HTML(code2html(sch.mod.script()))
+print(sch.mod.script())
+
+sch.reorder(j0, k, j1)
+# IPython.display.HTML(code2html(sch.mod.script()))
+print(sch.mod.script())
+
+block_C = sch.get_block("C", "mm_relu")
+sch.reverse_compute_at(block_C, j0)
+# IPython.display.HTML(code2html(sch.mod.script()))
+print(sch.mod.script())
+
+block_Y = sch.get_block("Y", "mm_relu")
+sch.decompose_reduction(block_Y, k)
+# IPython.display.HTML(code2html(sch.mod.script()))
+print(sch.mod.script())
+
+rt_lib = tvm.build(MyModule, target="llvm")
+a_nd = tvm.nd.array(a_np)
+b_nd = tvm.nd.array(b_np)
+c_nd = tvm.nd.empty((128, 128), dtype="float32")
+print(type(c_nd))
+func_mm_relu = rt_lib["mm_relu"]
+func_mm_relu(a_nd, b_nd, c_nd)
+
+np.testing.assert_allclose(c_mm_relu, c_nd.numpy(), rtol=1e-5)
+
+rt_lib_after = tvm.build(sch.mod, target="llvm")
+rt_lib_after["mm_relu"](a_nd, b_nd, c_nd)
+np.testing.assert_allclose(c_mm_relu, c_nd.numpy(), rtol=1e-5)
+
+f_timer_before = rt_lib.time_evaluator("mm_relu", tvm.cpu())
+print("Time cost of MyModule %g sec" % f_timer_before(a_nd, b_nd, c_nd).mean)
+f_timer_after = rt_lib_after.time_evaluator("mm_relu", tvm.cpu())
+print("Time cost of transformed sch.mod %g sec" % f_timer_after(a_nd, b_nd, c_nd).mean)
+
+# IPython.display.HTML(code2html(MyModule.script()))
+# IPython.display.HTML(code2html(sch.mod.script()))
+print(MyModule.script())
+print(sch.mod.script())
+
+# ==================== transform func ==================== #
+def transform(mod, jfactor):
+    sch = tvm.tir.Schedule(mod)
+    block_Y = sch.get_block("Y", func_name="mm_relu")
+    i, j, k = sch.get_loops(block_Y)
+    j0, j1 = sch.split(j, factors=[None, jfactor])
+    sch.reorder(j0, k, j1)
+    block_C = sch.get_block("C", "mm_relu")
+    sch.reverse_compute_at(block_C, j0)
+    return sch.mod
+
+mod_transformed = transform(MyModule, jfactor=8)
+
+rt_lib_transformed = tvm.build(mod_transformed, "llvm")
+f_timer_transformed = rt_lib_transformed.time_evaluator("mm_relu", tvm.cpu())
+print("Time cost of transformed mod_transformed %g sec" % f_timer_transformed(a_nd, b_nd, c_nd).mean)
+# display the code below
+# IPython.display.HTML(code2html(mod_transformed.script()))
+print(mod_transformed.script())
+
+# ==================== te ==================== #
+from tvm import te
+A = te.placeholder((128, 128), "float32", name="A")
+B = te.placeholder((128, 128), "float32", name="B")
+k = te.reduce_axis((0, 128), "k")
+Y = te.compute((128, 128), lambda i, j: te.sum(A[i, k] * B[k, j], axis=k), name="Y")
+C = te.compute((128, 128), lambda i, j: te.max(Y[i, j], 0), name="C")
+te_func = te.create_prim_func([A, B, C]).with_attr({"global_symbol": "mm_relu"})
+MyModuleFromTE = tvm.IRModule({"mm_relu": te_func})
+# IPython.display.HTML(code2html(MyModuleFromTE.script()))
+print(MyModuleFromTE.script())
